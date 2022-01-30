cb7bb1b00ae980a9e7e216b5a949b1b636cac8f1
Pander <pander@users.sourceforge.net>
2022-01-30 17:23:23 +0100

    minor

---
+++ _make_zips.sh
@@ -0,0 +1,4 @@
+rm -rf plugin*intergalacticfm.zip
+cd ..
+zip -r plugin.review.intergalacticfm/plugin.audio.intergalacticfm.zip plugin.audio.intergalacticfm -x \*git
+zip -r plugin.review.intergalacticfm/plugin.video.intergalacticfm.zip plugin.video.intergalacticfm -x \*git
